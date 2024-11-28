7a36b7d9b5bee75426ca278b05a48514e098cfd2
Sandroid Zotov <sasha.zotov.2002@mail.ru>
2024-11-28 22:09:30 +0300

    1 another version

---
+++ Inductance/Demo/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "../Domain/Inductance.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    try
+    {
+        Inductance inductance;
+        std::cin >> inductance;
+
+        std::cout << "Введённые данные: " << inductance << "\n";
+        std::cout << "Энергия магнитного поля: "
+            << inductance.CalculateEnergy() << " Дж\n";
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Ошибка: " << ex.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ Inductance/Tests/Tests.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Domain/Inductance.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace InductanceTests
+{
+    TEST_CLASS(InductanceTests)
+    {
+    public:
+        TEST_METHOD(TestCalculateEnergy)
+        {
+            Inductance inductance(0.5, 2);  // L = 0.5 Гн, I = 2 А
+            Assert::AreEqual(1.0, inductance.CalculateEnergy(), 1e-9);
+        }
+
+        TEST_METHOD(TestSetInductance)
+        {
+            Inductance inductance;
+            inductance.SetInductance(1.0);
+            Assert::AreEqual(1.0, inductance.GetInductance());
+        }
+
+        TEST_METHOD(TestNegativeInductance)
+        {
+            auto func = []() { Inductance inductance(-1.0, 1.0); };
+            Assert::ExpectException<std::invalid_argument>(func);
+        }
+    };
+}
