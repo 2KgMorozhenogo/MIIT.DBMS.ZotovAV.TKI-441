2adf25d11f34ed87cbdc0bb8f426fc441c8a9d16
Sandroid Zotov <sasha.zotov.2002@mail.ru>
2024-11-30 17:38:50 +0300

    Domain added

---
+++ Inductance/Domain/Inductance.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <iostream>
+#include <stdexcept>
+
+class Inductance
+{
+private:
+    double L;  // Индуктивность (Генри)
+    double I;  // Ток (Амперы)
+
+public:
+    Inductance(double inductance = 0.0, double current = 0.0);
+
+    void SetInductance(double inductance);
+    void SetCurrent(double current);
+    double GetInductance() const;
+    double GetCurrent() const;
+    double CalculateEnergy() const;
+
+    friend std::ostream& operator<<(std::ostream& out, const Inductance& inductance);
+    friend std::istream& operator>>(std::istream& in, Inductance& inductance);
+};
